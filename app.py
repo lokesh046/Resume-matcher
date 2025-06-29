7cb707282e3890ba7d4c01fcbfe498014dd9c4d8
lokesh046 <lokeshlearn@gmail.com>
2025-06-29 13:53:39 +0530

    fix: use dynamic port and reduce memory usage

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ def index():
     return render_template("index.html", score=None)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # use PORT env if set, else 5000
+    port = int(os.environ.get("PORT",12000))  # use PORT env if set, else 5000
     app.run(host="0.0.0.0", port=port)
     app.run(debug=False)
