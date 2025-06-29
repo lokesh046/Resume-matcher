81a01581dd6e48830336f811fba2c51298a203ca
lokesh046 <lokeshlearn@gmail.com>
2025-06-29 15:26:25 +0530

    first commi

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ def index():
 
     return render_template("index.html", score=None)
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",3000))  # use PORT env if set, else 5000
+    port = int(os.environ.get("PORT",12000))  # use PORT env if set, else 5000
     app.run(host="0.0.0.0", port=port)
     app.run(debug=False)
